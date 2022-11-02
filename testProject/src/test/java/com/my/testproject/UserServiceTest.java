3dfa8e6733d4492a7cb2eef6ac4581dffd87f722
Nikita Shapovalenko <71446610+NikSsh@users.noreply.github.com>
2022-11-02 14:26:39 +0200

    Fix testConfiguration

--- testProject/src/test/java/com/my/testproject/UserServiceTest.java
+++ testProject/src/test/java/com/my/testproject/UserServiceTest.java
@@ -6,18 +6,22 @@ import com.my.testproject.model.businessLayer.UserServiceImpl;
 import com.my.testproject.model.businessLayer.exception.UserNotFoundException;
 import com.my.testproject.model.persistenceLayer.UserRepository;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.context.assertj.ApplicationContextAssert;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -29,10 +33,10 @@ public class UserServiceTest {
 
         @Bean
         UserServiceImpl testUserService() {
-            User user = new User("testName", "testLastName", LocalDate.of(2000, 10, 10));
-            when(userRepository.findById(1L))
-                    .thenReturn(Optional.of(user));
-            when(userRepository.findById(2L))
+//            User user = new User("testName", "testLastName", LocalDate.of(2000, 10, 10));
+//            when(userRepository.findById(any()))
+//                    .thenReturn(Optional.of(user));
+            when(userRepository.findById(any()))
                     .thenReturn(Optional.empty());
             return new UserServiceImpl(userRepository);
         }
@@ -44,8 +48,7 @@ public class UserServiceTest {
     private UserService userService;
 
     @Test
-    void testGetUserById() {
-        assertNotNull(userService.getUserById(1L));
+    void testGetEmptyUserById() {
         assertThrows(UserNotFoundException.class, () -> userService.getUserById(2L));
     }
 }
